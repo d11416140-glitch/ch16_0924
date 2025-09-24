c690a8ffb22da8057969e905c3af683bd429fc57
d11416140-glitch <d11416140@gs.takming.edu.tw>
2025-09-24 11:08:14 +0800

    2

--- ch16/ch16/Program.cs
+++ ch16/ch16/Program.cs
@@ -6,9 +6,24 @@ namespace ch16
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.Read();
+            //Console.WriteLine("Hello World!");
+            //Console.Read();
+
+            // 輸入身高(公尺)
+            Console.Write("請輸入身高(公分): ");
+            double heightCm = Convert.ToDouble(Console.ReadLine());
+            double height = heightCm / 100;
+
+            // 輸入體重(公斤)
+            Console.Write("請輸入體重(公斤): ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            // 計算 BMI
+            double bmi = weight / (height * height);
+
+            // 輸出結果
+            Console.WriteLine("您的BMI值為: " + bmi);
+        }
+            }
 
         }
-    }
-}
