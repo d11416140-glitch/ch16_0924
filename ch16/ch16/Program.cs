52c42469fd7eb7718bfa6d2e440a569b788472e7
d11416140-glitch <d11416140@gs.takming.edu.tw>
2025-09-24 10:40:27 +0800

    1

---
+++ ch16/ch16/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ch16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+
+        }
+    }
+}
